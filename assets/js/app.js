db2f603fd035251a95cebd07ab72e3ca46d60b4d
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-05-10 09:21:09 +0530

    chnging font size in media screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,18 +73,19 @@ accordian.forEach(Element => {
     });
 });
 
-// first lick slider----------------
-$('.slider').slick({
+// first slick slider  ----------------
+
+$('.slick_slider').slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
         {
             breakpoint: 992,
             settings: {
-                slidesToShow: 5,
+                slidesToShow: 1,
                 slidesToScroll: 1,
                 infinite: true,
                 dots: true
@@ -93,14 +94,14 @@ $('.slider').slick({
         {
             breakpoint: 900,
             settings: {
-                slidesToShow: 4,
+                slidesToShow: 1,
                 slidesToScroll: 1
             }
         },
         {
             breakpoint: 768,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 1,
                 slidesToScroll: 1
             }
         },
@@ -108,7 +109,7 @@ $('.slider').slick({
         {
             breakpoint: 480,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 1,
                 slidesToScroll: 1
             }
         },
