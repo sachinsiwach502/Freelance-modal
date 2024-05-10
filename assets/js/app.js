d91115ad32888b1fe976770b8e76134630ec6a8c
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-05-10 11:27:29 +0530

    add font faimly on some text

--- assets/js/app.js
+++ assets/js/app.js
@@ -81,6 +81,8 @@ $('.slick_slider').slick({
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
     responsive: [
         {
             breakpoint: 992,
@@ -120,6 +122,39 @@ $('.slick_slider').slick({
                 slidesToScroll: 1
             }
         }
+    ]
+});
+
+
+// sec slider---------------------
+$('.poster_slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1.5,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
     ]
 });
 
